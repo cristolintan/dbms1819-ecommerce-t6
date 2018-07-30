1f164bb5cd09fd0dbecf940940366eda495a13d4
Crein13 <benz.matias13@gmail.com>
2018-07-30 18:16:12 +0800

    init1

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', function(req, res) {
-	res.render('home');
+	res.render('products');
 });
 
 app.get('/member/Gerald', function(req, res) {
