b31283cfe97e3630faf3de446b468d4b5b6e1a27
gtmabandos <gmabandos@gmail.com>
2018-08-07 10:21:28 +0800

    master

--- app.js
+++ app.js
@@ -138,7 +138,7 @@ app.get('/category/create', function(req, res) {
 	res.render('create_category');
 });
 
-app.post('/category/creating', function(req, res) {
+app.post('/category/create/saving', function(req, res) {
 	client.query("INSERT INTO categories (category_name) VALUES ('"+req.body.category+"')");
 	res.redirect('/categories');
 });
