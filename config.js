e48025541dcc7600a3b683a5a207a6a283738ad2
gtmabandos <gmabandos@gmail.com>
2018-08-07 08:43:06 +0800

    master

--- config.js
+++ config.js
@@ -13,11 +13,11 @@ const config = {
 	},
 	production: {
 		db: {
-			database: 'DB_DB',
-			user: 'DB_USER',
-			password: 'DB_PASSWORD',
-			host: 'DB_HOST',
-			port: 'DB_PORT',
+			database: process.env.DB_DB,
+			user: process.env.DB_USER,
+			password: process.env.DB_PASSWORD,
+			host: process.env.DB_HOST,
+			port: process.env.DB_PORT,
 			ssl: true
 		},
 		nodemailer: {
