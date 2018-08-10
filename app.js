1a3dc5658b44eacad1e9d54205eb6ed0364ba18d
Crein13 <benz.matias13@gmail.com>
2018-08-10 14:05:43 +0800

    try uli

--- app.js
+++ app.js
@@ -196,7 +196,7 @@ app.get('/product/update/:id', function(req,res) {
 	});
 	client.query("SELECT products.product_id AS product_id, products.product_name AS product_name, products.category_id AS category_id, products.brand_id AS brand_id, products.product_price AS product_price, products.product_description AS product_description, products.brand_tagline AS brand_tagline, products.product_picture AS product_picture, products.warranty AS warranty FROM products LEFT JOIN brands ON products.brand_id=brands.brand_id RIGHT JOIN categories ON products.category_id=categories.category_id WHERE products.product_id = '"+req.body.category_name+"'")
 	.then((result)=>{
-		product = result.rows;
+		product = result.rows[0];
 		console.log('product:', product);
 		both.push(product);
 	})
@@ -211,7 +211,7 @@ app.get('/product/update/:id', function(req,res) {
 		console.log(brand);
 		console.log(both);
 		res.render('update_product', {
-			rows: both
+			rows: results.rows[0]
 		});
 	})
 	.catch((err) => {
