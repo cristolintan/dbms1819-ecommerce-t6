d7da77f9a3a85a847b91f847566a9714c75a1b03
Crein13 <benz.matias13@gmail.com>
2018-08-10 11:24:43 +0800

    try uli

--- app.js
+++ app.js
@@ -168,7 +168,7 @@ app.get('/product/create', function(req, res) {
 
 app.post('/product/create/saving', function(req,res) {
 	var description = req.body.product_description;
-	client.query("INSERT INTO products (product_picture,product_name,product_description,brand_tagline,product_price,warranty,category_id,brand_id) VALUES ('"+req.body.product_picture+"','"+req.body.product_name+"','description','"+req.body.brand_tagline+"','"+req.body.product_price+"','"+req.body.warranty+"','"+req.body.category_id+"','"+req.body.brand_id+"')")
+	client.query("INSERT INTO products (product_picture,product_name,product_description,brand_tagline,product_price,warranty,category_id,brand_id) VALUES ('"+req.body.product_picture+"','"+req.body.product_name+"',description,'"+req.body.brand_tagline+"','"+req.body.product_price+"','"+req.body.warranty+"','"+req.body.category_id+"','"+req.body.brand_id+"')")
 	.then(result=>{
 		console.log('results?', result);
 		res.redirect('/');
